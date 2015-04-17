acff8ebc84ea8dcf39e305cbae6609504a057117
Lu Cao <caoool.link@icloud.com>
2015-04-18 04:38:43 +0800

    Initial Commit

---
+++ WatchSmile/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  WatchSmile
+//
+//  Created by Lu Cao on 4/18/15.
+//  Copyright (c) 2015 LoopCow. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ WatchSmile/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  WatchSmile
+//
+//  Created by Lu Cao on 4/18/15.
+//  Copyright (c) 2015 LoopCow. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
